10fdc67be419a294456315016b79887b4d7cb5b7
henricastt30 <moreirahenrique3010@gmail.com>
2025-05-26 22:22:06 -0300

    Feat: introdução ao React

---
+++ ola/src/components/Body.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Body.css'
+
+function Body() {
+  return (
+    <div className='Body'>
+        <h2 className='titulo2'>Dinos são legais</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. In ratione voluptatibus sunt et perspiciatis aperiam incidunt, dolorem nisi iusto autem reprehenderit culpa placeat suscipit provident est fugiat dolor voluptates aliquid!</p>
+    </div>
+  )
+}
+
+export default Body
